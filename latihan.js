10f80bd22eb2ee71b00b694496fda1139a1bf502
GamalielFeliksFeranta <gferanta@student.ciputra.ac.id>
2023-06-11 02:14:38 +0700

    mencoba while dan popup box pada JS

---
+++ latihan.js
@@ -0,0 +1,24 @@
+// alert("hai");
+// alert("saya belajar javascript");
+// var x=10;
+// console.log("hai");
+// console.log("isi dari variable X adalah "+x);
+// // alert("isi dari variable X adalah "+x);
+// var nama=prompt("masukkan nama:");
+// alert("nama saya adalah "+nama);
+// console.log("nama saya adalah "+nama);
+// var answ = confirm("are u sure?");
+// if (answ == true){
+//     alert("user menekan tombol ok");
+// }else{
+//     alert("user menekan tombol cancel");
+// }
+alert ("selamat datang");
+var lagi =true;
+while(lagi ){
+    var nama = prompt("masukan nama: ");
+    alert("halo "+nama);
+    lagi = confirm("coba lagi?");
+
+}
+alert("terimakasih");
