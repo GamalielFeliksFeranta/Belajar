1261cf52b57435cab934e5513d9a5bc51b7965c6
GamalielFeliksFeranta <gferanta@student.ciputra.ac.id>
2023-06-11 19:25:30 +0700

    belajar menggunakan for lebih detail

---
+++ angkot3.js
@@ -0,0 +1,27 @@
+var jumlahAngkot=10;
+var angkotBeroperasi=6;
+var noAngkot=1;
+
+
+for(noAngkot ; noAngkot<=jumlahAngkot; noAngkot++)
+{   
+    if(noAngkot<=angkotBeroperasi && noAngkot<jumlahAngkot)
+    console.log("Angkot No. "+noAngkot+" beroperasi dengan baik.")  
+else
+    {
+        console.log("Angkot No. "+noAngkot+" tidak beroperasi dengan baik.")  
+    } 
+}
+
+
+
+// var angka =prompt("masukkan angka :");
+// if(angka % 2 ==0)
+// {
+//     alert(angka+ " adalah bilangan Genap")
+// }
+// alert(angka+ " adalah bilangan Ganjil")
+
+
+
+
